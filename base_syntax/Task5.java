2e0a0b9d3342a5285d8db78440b96834a9e76afd
UvarovIV <post689@mail.ru>
2022-06-21 19:04:12 +0300

    changed file Task5.java

--- base_syntax/Task5.java
+++ base_syntax/Task5.java
@@ -1,27 +1,15 @@
 package base_syntax;
 
 public class Task5 {
+    public static void easyNumbers(int num){
+        for (int i = 2; i < num; i++) 
+            if (num % i == 0)
+                return;
+        System.out.print(num + " ");
+    }
+
     public static void main(String[] args) {
-        int[] array = new int[5];
-
-        for (int i = 0; i < array.length; i++)
-            array[i] = (int) (Math.random() * 99) + 2;
-
-        for (int i = 0; i < array.length; i++)
-            System.out.print(array[i] + " ");
-        
-        int max = array[0];
-        int sum = 0;
-
-        for (int i = 0; i < array.length; i++){
-            if(array[i] > max)
-                max = array[i];
-            sum += array[i];
-        }
-        
-        System.out.println();
-        System.out.println("Максимальное значение в массиве " + max);
-        System.out.println("Среднее значение " + (double) sum / array.length);
-            
+        for (int i = 2; i < 101; i++)
+            easyNumbers(i);
     }
 }
