da71a77ed068f0363a97dbeb23203ed6e249952f
UvarovIV <post689@mail.ru>
2022-06-21 19:10:41 +0300

    changed name of function "easyNumbers" -> "primes"

--- base_syntax/Task5.java
+++ base_syntax/Task5.java
@@ -1,7 +1,7 @@
 package base_syntax;
 
 public class Task5 {
-    public static void easyNumbers(int num){
+    public static void primes(int num){
         for (int i = 2; i < num; i++) 
             if (num % i == 0)
                 return;
@@ -10,6 +10,6 @@ public class Task5 {
 
     public static void main(String[] args) {
         for (int i = 2; i < 101; i++)
-            easyNumbers(i);
+            primes(i);
     }
 }
