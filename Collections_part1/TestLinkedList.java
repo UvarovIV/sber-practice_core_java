2e9a59d0e2c9e516070b45753414b85f5cfaedc8
UvarovIV <post689@mail.ru>
2022-06-27 18:44:39 +0300

    extended TestLinkedList.java

--- Collections_part1/TestLinkedList.java
+++ Collections_part1/TestLinkedList.java
@@ -1,10 +1,10 @@
 package Collections_part1;
 
-
-
 public class TestLinkedList {
     public static void main(String[] args) {
+
         LinkedList ll = new LinkedList();
+
         ll.addLast(3);
         ll.addLast(1);
         ll.addLast(1);
@@ -14,58 +14,55 @@ public class TestLinkedList {
         ll.add(0, 12);
         ll.add(ll.size(), 10);
         ll.add(3, 15);
-        System.out.println("Size: " + ll.size());
+        ll.add(2);
 
+        System.out.println("\nTest add");
         for (var item : ll) 
-            System.out.println(item);
+            System.out.print(item + " ");
 
-        LinkedList sublist = ll.subList(0, 9); 
-        System.out.println("Size: " + ll.size());
+        LinkedList sublist = ll.subList(3, 6); 
 
-        System.out.println();
-
+        System.out.println("\n\nTest sublist from 3 to 6");
         for (var item : sublist) 
-            System.out.println(item);
+            System.out.print(item + " ");
 
-        System.out.println();
+        System.out.println("\n\nTest get");
         System.out.println("First Element: " + ll.getFirst());
         System.out.println("Last Element: " + ll.getLast());
-        System.out.println("Size: " + ll.size());
+        System.out.println("Element with index 3: " + ll.get(3));
 
-        System.out.println();
+        System.out.println("\nTest indexOf");
+        System.out.println("First Index of 1: " + ll.indexOf(1));
+        System.out.println("Last Index of 1: " + ll.lastIndexOf(1));
 
+        System.out.println("\nTest poll");
         System.out.println("First Element deleted: " + ll.pollFirst());
         System.out.println("Last Element deleted: " + ll.pollLast());
-        System.out.println("Size: " + ll.size());
-        System.out.println("First Element deleted: " + ll.pollFirst());
-        System.out.println("Last Element deleted: " + ll.pollLast());
-        System.out.println("Size: " + ll.size());
 
-        System.out.println();
-
-        for (var item : ll) 
-            System.out.println(item);
-
-        System.out.println();
-
+        System.out.println("\nTest remove");
         System.out.println("First Element deleted: " + ll.removeFirst());
         System.out.println("Last Element deleted: " + ll.removeLast());
-        System.out.println("Size: " + ll.size());
+        System.out.println("Remove element with index 4: " + ll.remove(4));
+        System.out.println("Remove element 1: " + ll.remove((Object) 1 ));
         
-        System.out.println();
+        System.out.println("\nAfter removing elements");
         for (var item : ll) 
-            System.out.println(item);
+            System.out.print(item + " ");
 
-        System.out.println(ll.contains(9));
-        System.out.println(ll.contains(3));
-        System.out.println(ll.contains(5));
-        System.out.println(ll.contains(7));
-        System.out.println(ll.contains(0));
-        System.out.println("Size: " + ll.size());
+        System.out.println("\nTest set");
+        ll.set(0, 1);
+        for (var item : ll) 
+            System.out.print(item + " ");
+
+        System.out.println("\nLinked list contains 9: " + ll.contains(9));
+        System.out.println("Linked list contains 3: " + ll.contains(3));
+        System.out.println("Linked list contains 1: " + ll.contains(1));
+        System.out.println("Linked list contains 0: " + ll.contains(0));
         
-        System.out.println(ll.isEmpty());
+        System.out.println("\nLinked list is empty: " + ll.isEmpty());
         ll.clear();
-        System.out.println(ll.isEmpty());
-        System.out.println(ll.size());
+        System.out.println("Linked list is empty: " + ll.isEmpty());
+        
+
     }
 }
